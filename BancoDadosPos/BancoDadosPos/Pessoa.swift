215f0145b2725fd13d0e257e958e2777860312a7
Faculdade Alfa <faculdadealfa@Mac-07.local>
2018-03-24 08:36:42 -0300

    primeiro comit do banco pos

---
+++ BancoDadosPos/BancoDadosPos/Pessoa.swift
@@ -0,0 +1,19 @@
+//
+//  Pessoa.swift
+//  BancoDadosPos
+//
+//  Created by Faculdade Alfa on 24/03/2018.
+//  Copyright © 2018 Faculdade Alfa. All rights reserved.
+//
+
+import UIKit
+
+class Pessoa {
+    var nome: String
+    var cidade: String
+    
+    init(nome: String, cidade: String) {
+        self.nome = nome
+        self.cidade = cidade
+    }
+}
---
+++ BancoDadosPos/BancoDadosPos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BancoDadosPos
+//
+//  Created by Faculdade Alfa on 24/03/2018.
+//  Copyright © 2018 Faculdade Alfa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
