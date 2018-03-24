79727ea81c30ad4b26ff2f6203e4ff62d4442361
Faculdade Alfa <faculdadealfa@Mac-07.local>
2018-03-24 10:24:20 -0300

    função buscar

--- BancoDadosPos/BancoDadosPos/TbPessoa.swift
+++ BancoDadosPos/BancoDadosPos/TbPessoa.swift
@@ -14,8 +14,44 @@ class TbPessoa{
     private let nomeTabela = "TblPessoa"
     private var pessoas:[NSManagedObject] = []
     
-    func buscar(){
+    func buscar(filtro: NSPredicate?) -> [Pessoa]{// ? pode ser nulo
+        pessoas = []
+        var listaPessoa = [Pessoa]()
         
+        //carregando o AppDelegate da aplicação, se retornar nulo, para a execução
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return listaPessoa
+        }
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //carregando a estrutura da tabela do model
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: nomeTabela)
+        
+        do {
+            //verifica se existe filtro, se sim, adiciona o filtro na tabela
+            if let condicao = filtro {
+                fetchRequest.predicate = condicao
+            }
+            
+            //faz a busca dos dados da tabela com os filtros
+            pessoas = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Deu Erro: \(error). \(error.userInfo)")
+        }
+        
+        for registro in pessoas{
+            let nome = registro.value(forKey: "nome") as! String // as! String = retornando tipo String
+            let cidade = registro.value(forKey: "cidade") as! String
+            
+            //criando um objeto pessoa com os dados do banco de dados
+            let pes = Pessoa(nome: nome, cidade: cidade)
+            
+            //adicionando a pessoa na lista de retorno
+            listaPessoa.append(pes)
+        }
+        
+        return listaPessoa
     }
     
     func salvar() {
@@ -26,3 +62,4 @@ class TbPessoa{
         
     }
 }
+
