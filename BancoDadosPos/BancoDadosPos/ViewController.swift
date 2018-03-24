d32ba0b9313b7221feb8973d60b2a0bb0d9d9942
Faculdade Alfa <faculdadealfa@Mac-07.local>
2018-03-24 14:59:15 -0300

    recarregar tela após salvar

--- BancoDadosPos/BancoDadosPos/ViewController.swift
+++ BancoDadosPos/BancoDadosPos/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var campoCidade: UITextField!
     @IBOutlet weak var tabela: UITableView!
     @IBAction func salvar(_ sender: Any) {
+        //salvando os dados da tela no banco de dados
         tbPessoa.salvar(i: -1, nome: campoNome.text!, cidade: campoCidade.text!)
+        
+        //recarregar os dados após salvar
+        tabela.reloadData()
         
         //limpar campos apos salvar
         campoCidade.text = ""
