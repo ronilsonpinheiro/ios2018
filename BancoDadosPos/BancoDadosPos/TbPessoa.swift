e51c13ac04f0002db92c4e4132e389d6e34c7646
Faculdade Alfa <faculdadealfa@Mac-07.local>
2018-03-24 08:41:52 -0300

    inserindo framework DB

---
+++ BancoDadosPos/BancoDadosPos/TbPessoa.swift
@@ -0,0 +1,13 @@
+//
+//  TbPessoa.swift
+//  BancoDadosPos
+//
+//  Created by Faculdade Alfa on 24/03/2018.
+//  Copyright © 2018 Faculdade Alfa. All rights reserved.
+//
+
+import UIKit
+
+class TbPessoa{
+    
+}
