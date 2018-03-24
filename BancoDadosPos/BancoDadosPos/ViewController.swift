9c49ae23a121038ced1c25319f6655f1e448668a
Faculdade Alfa <faculdadealfa@Mac-07.local>
2018-03-24 13:17:20 -0300

    limpar campos da tela após salvar

--- BancoDadosPos/BancoDadosPos/ViewController.swift
+++ BancoDadosPos/BancoDadosPos/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var tabela: UITableView!
     @IBAction func salvar(_ sender: Any) {
         tbPessoa.salvar(i: -1, nome: campoNome.text!, cidade: campoCidade.text!)
+        campoNome.text = ""
+        campoCidade.text = ""
     }
     
     var tbPessoa = TbPessoa()
@@ -24,12 +26,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
